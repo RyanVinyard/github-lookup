7ada27806454eb2e96f68bbdbb662d4e8768c669
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-10-17 06:14:05 -0700

    Add functionality for username and repository search

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,18 +1,23 @@
-var User = require("./../js/search.js").searchModule;
+var Search = require("./../js/search.js").searchModule;
 
-var userNameDisplay = function(username, name) {
+var userNameDisplay = function(userName, name) {
   $("#results").append("<h2>" + name + "</h2>");
 };
 
-// var userFail = function(message) {
-//   $("#results").append("<h2>Unfortunately, ")
-// }
+var userFail = function(message) {
+  $("#results").append("<h2>Unfortunately, " + message);
+}
+
+var displayRepositories = function(userName, description, url) {
+  $("#repositories").append("<ul>" + "<li>" + userName + "</li>" + "<li>" + description + "</li>" + "<li>" + url + "</li>" + "</ul>");
+};
 
 $(document).ready(function() {
   $("#search-form").submit(function(event) {
     event.preventDefault();
-    var username = $("#search").val();
-    var newUser = new User(username);
+    var userName = $("#search").val();
+    var newUser = new User(userName);
     newUser.getUser(userNameDisplay, userFail);
+    newUser.getRepositories(displayRepositories);
   });
 });
--- js/search.js
+++ js/search.js
@@ -5,9 +5,26 @@ function Search(username) {
 }
 
 Search.prototype.getUser = function(userNameDisplay, userFail) {
+  var = this.username;
   $.get("https://api.github.com/users/" + username + "?access_token=" + apiKey).then(function(response) {
     userNameDisplay(username, response.name);
   }).fail(function(error) {
     userFail(username, error.responseJSON.message);
   });
 };
+
+Search.prototype.getRepositories = function(displayRepositories) {
+  var username = this.username;
+  $.get("https://api.github.com/users/" + username + "/repos?access_token=" + apiKey).then(function(response) {
+    for(var i=0; i<response.length; i++) {
+      if (response[i].description !== null) {
+        displayRepositories(response[i].userName, response[i].description, response[i].url);
+      } else {
+        displayRepositories(response[i].userName, "None", response[i].url);
+      }
+    }
+  }).fail(function(error)) {
+    alert(response);
+  });
+};
+exports.searchModule = Search;
